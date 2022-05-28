8dd5b9c7d4255da3fbe173cfd3919a9502c6f3fa
timmy_3000 <66499629+timmy3000@users.noreply.github.com>
2022-05-28 20:39:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 class Student:
-    # [assignment] Skeleton class. Add your code here
     def __init__(self, name = "", age = 0, tracks = [""], score = 0.0):
         self.name = str(name)
+        self.age = int(age)
+        self.tracks = []
 
-        try:
-            self.age = int(age)
-        except Exception:
-            return Exception
+        if(isinstance(tracks, list)):
+            self.tracks = self.tracks + tracks
+        else:
+            self.tracks.append(tracks)
 
-        self.tracks = list(tracks)
 
-        try:
-            self.score = float(score)
-        except Exception:
-            return Exception
+        self.score = float(score)
     
 
     # getters
     def get_name(self):
+        print("Name:", self.name)
         return self.name
 
     def get_age(self):
+        print("Age:", self.age)
         return self.age
 
     def get_tracks(self):
+        print("Tracks:", self.tracks)
         return self.tracks
 
     def get_score(self):
+        print("Score:", self.score)
         return self.score
 
 
@@ -34,20 +35,29 @@ class Student:
     def change_name(self, name):
         self.name = str(name)
 
+
     def change_age(self, age):
         try:
             self.age = int(age)
         except Exception:
             return Exception
 
+
     def add_track(self, tracks):
-        self.tracks = list(tracks)
+        self.tracks = []
+
+        if(isinstance(tracks, list)):
+            self.tracks = self.tracks + tracks
+        else:
+            self.tracks.append(tracks)
+
 
     def change_score(self, score):
         try:
             self.score = float(score)
         except Exception:
             return Exception
+            
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
@@ -56,4 +66,4 @@ Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
-print("Score: " + str(Bob.get_score()))
+Bob.get_score()
